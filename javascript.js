e6d2e891ef7d26d38c6f4ca19b84061cc19383fb
Divakar Gupta <divakargupta13@gmail.com>
2024-11-21 16:59:13 +0530

    First commit in rps-ui branch

--- javascript.js
+++ javascript.js
@@ -5,55 +5,136 @@ function getComputerChoice() {
     else return "Scissor";
 }
 
-function getHumanChoice() {
-    return prompt("What is your shape?");
-}
-
 let humanScore = 0, computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     if(humanChoice == "Rock") {
         if(computerChoice == "Paper") {
             computerScore++;
-            console.log("You lose! Paper beats Rock");
+            let list = document.querySelector("ol");
+            let span = document.createElement("span");
+            span.textContent = "You lose! Paper beats Rock";
+            let li = document.createElement("li");
+            li.appendChild(span);
+            list.appendChild(li);
+
         }
         else if(computerChoice == "Scissor") {
             humanScore++;
-            console.log("You Win! Rock beats Scissor");
+            let list = document.querySelector("ol");
+            let span = document.createElement("span");
+            span.textContent = "You Win! Rock beats Scissor";
+            let li = document.createElement("li");
+            li.appendChild(span);
+            list.appendChild(li);
         }
-        else console.log("Match tied");
+        else {
+            let list = document.querySelector("ol");
+            let span = document.createElement("span");
+            span.textContent = "Match tied";
+            let li = document.createElement("li");
+            li.appendChild(span);
+            list.appendChild(li);
+        }
     }
     if(humanChoice == "Paper") {
         if(computerChoice == "Rock") {
             humanScore++;
-            console.log("You Win! Paper beats Rock");
+            let list = document.querySelector("ol");
+            let span = document.createElement("span");
+            span.textContent = "You Win! Paper beats Rock";
+            let li = document.createElement("li");
+            li.appendChild(span);
+            list.appendChild(li);
         }
         else if(computerChoice == "Scissor") {
             computerScore++;
-            console.log("You Lose! Scissor beats Paper");
+            let list = document.querySelector("ol");
+            let span = document.createElement("span");
+            span.textContent = "You Lose! Scissor beats Paper";
+            let li = document.createElement("li");
+            li.appendChild(span);
+            list.appendChild(li);
         }
-        else console.log("Match tied");
+        else {
+            let list = document.querySelector("ol");
+            let span = document.createElement("span");
+            span.textContent = "Match tied";
+            let li = document.createElement("li");
+            li.appendChild(span);
+            list.appendChild(li);
+        }
     }
     if(humanChoice == "Scissor") {
         if(computerChoice == "Paper") {
             humanScore++;
-            console.log("You Win! Scissor beats Paper");
+            let list = document.querySelector("ol");
+            let span = document.createElement("span");
+            span.textContent = "You Win! Scissor beats Paper";
+            let li = document.createElement("li");
+            li.appendChild(span);
+            list.appendChild(li);
         }
         else if(computerChoice == "Rock") {
             computerScore++;
-            console.log("You Lose! Rock beats Paper");
+            let list = document.querySelector("ol");
+            let span = document.createElement("span");
+            span.textContent = "You Lose! Rock beats Scissor";
+            let li = document.createElement("li");
+            li.appendChild(span);
+            list.appendChild(li);
         }
-        else console.log("Match tied");
+        else {
+            let list = document.querySelector("ol");
+            let span = document.createElement("span");
+            span.textContent = "Match tied";
+            let li = document.createElement("li");
+            li.appendChild(span);
+            list.appendChild(li);
+        }
     }
 }
 
-function playGame() {
-    for(let i = 0; i < 5; i++) {
-        let humanChoice = getHumanChoice();
-        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-        const computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
+function updateScores(humanScore, computerScore) {
+    const yourScoreSpan = document.querySelector('ul li:nth-child(1) span');
+    const computerScoreSpan = document.querySelector('ul li:nth-child(2) span');
+    yourScoreSpan.textContent = humanScore;
+    computerScoreSpan.textContent = computerScore;
 }
 
-playGame();
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissor = document.getElementById("scissor");
+
+rock.addEventListener('click', function(){
+    let humanChoice = "Rock";
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    updateScores(humanScore, computerScore);
+    if(humanScore == 5) announceWinner("You");
+    else if(computerScore == 5) announceWinner("Computer");
+});
+paper.addEventListener('click', function(){
+    let humanChoice = "Paper";
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    updateScores(humanScore, computerScore);
+    if(humanScore == 5) announceWinner("You");
+    else if(computerScore == 5) announceWinner("Computer");
+});
+scissor.addEventListener('click', function(){
+    let humanChoice = "Scissor";
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    updateScores(humanScore, computerScore);
+    if(humanScore == 5) announceWinner("You");
+    else if(computerScore == 5) announceWinner("Computer");
+});
+
+function announceWinner(winner) {
+    alert(winner + " wins the game!");
+    // Reset scores
+    yourScore = 0;
+    computerScore = 0;
+    updateScores();
+}
